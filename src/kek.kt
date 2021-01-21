c3d611c2e246f598ac538dea4c090f5a6ddaae52
MIV228 <mivgithub@gmail.com>
2021-01-21 18:41:42 +0700

    rtr

---
+++ src/kek.kt
@@ -0,0 +1,12 @@
+fun main () {
+    print ("Скорость первого автомобиля:" )
+    var V1 = readLine().toString()!!.toDouble()
+    print ("Скорость второго автомобиля:" )
+    var V2 = readLine().toString()!!.toDouble()
+    print ("Расстояние на момент начала рассчета:" )
+    var Sbegin = readLine().toString()!!.toDouble()
+    print ("Время прошедшее со старта:" )
+    var T = readLine().toString()!!.toDouble()
+    var Sfinal = Sbegin+V1*T+V2*T
+    print (Sfinal)
+}
