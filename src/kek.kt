36b78d29c46d47ce18a93cb303937bbd02d549d0
MIV228 <mivgithub@gmail.com>
2021-01-21 18:51:55 +0700

    Программа для расчета S между 2 автомобилями через t часов.

--- src/kek.kt
+++ src/kek.kt
@@ -1,12 +1,13 @@
 fun main () {
-    print ("Скорость первого автомобиля:" )
+    println("Сейчас мы находим S между автомобилями через t часов.")
+    print ("Скорость первого автомобиля(В км/ч):" )
     var V1 = readLine().toString()!!.toDouble()
-    print ("Скорость второго автомобиля:" )
+    print ("Скорость второго автомобиля(В км/ч):" )
     var V2 = readLine().toString()!!.toDouble()
-    print ("Расстояние на момент начала рассчета:" )
+    print ("Расстояние на момент начала рассчета(В километрах):" )
     var Sbegin = readLine().toString()!!.toDouble()
-    print ("Время прошедшее со старта:" )
-    var T = readLine().toString()!!.toDouble()
-    var Sfinal = Sbegin+V1*T+V2*T
+    print ("Время прошедшее со старта(В часах):" )
+    var t = readLine().toString()!!.toDouble()
+    var Sfinal = Sbegin+V1*t+V2*t
     print ("Итоговый рассчет: $Sfinal" )
 }
